56e74f320fcb419290f78853eea8d108895e8d20
zoz <zeroexzoz@gmail.com>
2022-03-29 09:21:39 +1100

    add changes

--- src/js/sharedEth/utils.js
+++ src/js/sharedEth/utils.js
@@ -69,7 +69,7 @@ function providerTypeId(provider) {
   } else if (provider.isImToken) {
     return 3;
   }else if (provider.isTally) {
-    return 3;
+    return 5;
   } else if (provider.isMetaMask) {
     if (provider.constructor.name === 'InpageBridge') {
       return 3;
